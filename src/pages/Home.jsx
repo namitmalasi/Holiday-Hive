c97fb26c09e191553372efa6c7da03344259d6a1
Namit Malasi <namitmalasi@gmail.com>
2026-09-25 18:49:23 +0530

    chore: initialize HolidayHive travel website

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <main>
+      <h1 className="text-4xl font-bold">HolidayHive</h1>
+      <p>Discover places you'll never forget.</p>
+    </main>
+  );
+}
+
+export default Home;
